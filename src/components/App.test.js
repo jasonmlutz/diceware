67e4df5eb3cf31667f514dcd73bb58f653a7f22a
Jason Lutz <jason.m.lutz@gmail.com>
2022-07-29 13:17:18 -0700

    Move App and update test

---
+++ src/components/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("renders app path", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/src\/app\.js/i);
+  expect(linkElement).toBeInTheDocument();
+});
