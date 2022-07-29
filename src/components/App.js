0fd351159594f38f091756ed080f64ed96c488fb
Jason Lutz <jason.m.lutz@gmail.com>
2022-07-29 13:55:36 -0700

    Implement InputField

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [formData, setFormData] = useState({
+    first: "",
+    second: "",
+    third: "",
+    fourth: "",
+    fifth: "",
+    sixth: "",
+  });
+
+  let InputField = ({ name }) => {
+    return (
+      <label>
+        {name}
+        <input
+          type="text"
+          className="p-2 m-2 border border-black rounded-md"
+          value={formData[name]}
+          onChange={(e) =>
+            setFormData((prevState) => {
+              return { ...prevState, [name]: e.target.value };
+            })
+          }
+        />
+      </label>
+    );
+  };
+
+  const renderInputFields = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+  ].map((name, i) => (
+    <div key={i}>
+      <InputField name={name} />
+    </div>
+  ));
+
   return (
     <div className="container border border-black rounded-md mx-auto m-2 p-2">
       <p className="font-bold">src/App.js</p>
+      <div>{renderInputFields}</div>
     </div>
   );
 };
