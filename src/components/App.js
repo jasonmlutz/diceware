a45aefa40fb2a88b418754dd96c62c72e812c715
Jason Lutz <jason.m.lutz@gmail.com>
2022-07-29 14:31:13 -0700

    Implement initial submit logic

--- src/components/App.js
+++ src/components/App.js
@@ -20,22 +20,19 @@ const InputField = ({ name, state, setState }) => {
 
 const App = () => {
   const [formData, setFormData] = useState({
-    first: "",
-    second: "",
-    third: "",
-    fourth: "",
-    fifth: "",
-    sixth: "",
+    first: "12345",
+    second: "54321",
+    third: "23415",
+    fourth: "34153",
+    fifth: "22413",
+    sixth: "52134",
   });
 
-  const renderInputFields = [
-    "first",
-    "second",
-    "third",
-    "fourth",
-    "fifth",
-    "sixth",
-  ].map((name, i) => {
+  const [passphrase, setPassphrase] = useState("");
+
+  const numerals = ["first", "second", "third", "fourth", "fifth", "sixth"];
+
+  const renderInputFields = numerals.map((name, i) => {
     const props = { name, state: formData, setState: setFormData };
     return (
       <div key={i}>
@@ -44,10 +41,45 @@ const App = () => {
     );
   });
 
+  const digits = "12345".split("");
+
+  function canSubmit() {
+    return Object.values(formData).every((input) => {
+      return (
+        input.length === 5 &&
+        input.split("").every((char) => digits.includes(char))
+      );
+    });
+  }
+
+  function handleSubmit() {
+    if (canSubmit()) {
+      setPassphrase(generatePassphrase());
+    } else {
+      setPassphrase("input data not valid");
+    }
+  }
+
+  function generatePassphrase() {
+    let phrase = [];
+    for (let n of numerals) {
+      let d = formData[n];
+      phrase.push(d);
+    }
+    return phrase.join(" ");
+  }
+
   return (
     <div className="container border border-black rounded-md mx-auto m-2 p-2">
       <p className="font-bold">src/App.js</p>
       <div>{renderInputFields}</div>
+      <button
+        className="font-bold border border-black rounded-md p-2 m-2 hover:bg-slate-200"
+        onClick={handleSubmit}
+      >
+        submit
+      </button>
+      {passphrase}
     </div>
   );
 };
