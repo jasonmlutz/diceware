71d9ff308d53e72ae0f907a57a7e860b3e5153e0
Jason Lutz <jason.m.lutz@gmail.com>
2022-07-29 20:37:51 -0700

    Improve styling, add copy and warning
    This commit substantially styles the App, adding media queries, centering, and overall visual improvement.
    We add a brief warning as a footer regarding
    cryptographic security.
    
    This commit also adds a copy passphrase feature
    using the `navigator.clipboard.writeText` API.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Helmet, HelmetData } from "react-helmet-async";
+import { MdContentCopy } from "react-icons/md";
 
 import { words } from "../resources/words";
+
+const helmetData = new HelmetData({});
 
 let wordData = {};
 for (let i = 0; i + 1 < words.length; i++) {
@@ -9,19 +13,17 @@ for (let i = 0; i + 1 < words.length; i++) {
 
 const InputField = ({ name, state, setState }) => {
   return (
-    <label>
-      {name}
-      <input
-        type="text"
-        className="p-2 m-2 border border-black rounded-md"
-        value={state[name]}
-        onChange={(e) =>
-          setState((prevState) => {
-            return { ...prevState, [name]: e.target.value };
-          })
-        }
-      />
-    </label>
+    <input
+      type="text"
+      className="px-6 mb-2 mx-auto font-mono text-4xl placeholder-text-2xl w-40 rounded-lg border-transparent appearance-none border border-gray-300 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+      placeholder={name.toUpperCase()}
+      value={state[name]}
+      onChange={(e) =>
+        setState((prevState) => {
+          return { ...prevState, [name]: e.target.value };
+        })
+      }
+    />
   );
 };
 
@@ -81,9 +83,9 @@ const App = () => {
   const renderInputFields = numerals.map((name, i) => {
     const props = { name, state: formData, setState: setFormData };
     return (
-      <div key={i}>
+      <li key={i} className="">
         <InputField {...props} />
-      </div>
+      </li>
     );
   });
 
@@ -115,32 +117,77 @@ const App = () => {
     return phrase.join(" ");
   }
 
+  function handleCopy() {
+    navigator.clipboard.writeText(passphrase);
+    window.alert("passphrase copied to clipboard");
+  }
+
+  const buttonClasses =
+    "p-1 m-1 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg w-40";
+
   return (
-    <div className="container border border-black rounded-md mx-auto m-2 p-2">
-      <p className="font-bold">src/App.js</p>
-      <div>{renderInputFields}</div>
-      <div>
-        <button
-          className="font-bold border border-black rounded-md p-2 m-2 hover:bg-slate-200"
-          onClick={randomizeData}
-        >
-          randomize
-        </button>
-        <button
-          className="font-bold border border-black rounded-md p-2 m-2 hover:bg-slate-200"
-          onClick={handleSubmit}
-        >
-          submit
-        </button>
-        <button
-          className="font-bold border border-black rounded-md p-2 m-2 hover:bg-slate-200"
-          onClick={clearFormData}
-        >
-          clear
-        </button>
+    <>
+      <Helmet helmetData={helmetData}>
+        <title>Passphrase Generator</title>
+      </Helmet>
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+          className="absolute h-full w-full object-cover"
+          alt="background mountain landscape"
+        />
+        <div className="absolute z-20 inset-x-4 bottom-4 text-white text-xs pt-5 px-4">
+          <p>
+            <strong>Warning: </strong>
+            This tool is for educational purposes only. Do not use this tool for
+            anything related to security, as it does not provide
+            cryptographically secure random numbers.
+          </p>
+        </div>
+        <div className="inset-0 bg-black opacity-70 absolute"></div>
+        <div className="container mx-auto relative z-10 mt-5 sm:mt-10 md:mt-20">
+          <div className="flex flex-col content-center relative z-10">
+            <div className="text-2xl text-white uppercase mx-auto mb-3">
+              Passphrase Generator
+            </div>
+            <ul className="flex flex-col flex-nowrap w-full items-center">
+              {renderInputFields}
+            </ul>
+            <div className="flex w-full my-2 sm:my-4 flex-wrap sm:flex-nowrap justify-center">
+              <button className={buttonClasses} onClick={randomizeData}>
+                randomize
+              </button>
+              <button
+                className={
+                  buttonClasses +
+                  (!canSubmit()
+                    ? " cursor-not-allowed bg-slate-800 hover:bg-slate-900"
+                    : "")
+                }
+                onClick={handleSubmit}
+                disabled={!canSubmit()}
+              >
+                submit
+              </button>
+              <button className={buttonClasses} onClick={clearFormData}>
+                clear
+              </button>
+            </div>
+            {passphrase.length ? (
+              <div className="relative mx-auto mt-5 w-4/5 md:w-1/2 text-black rounded-md font-bold mx-auto text-l sm:text-xl font-mono bg-slate-200 p-2 sm:p-4">
+                {passphrase}
+                <div
+                  className="cursor-pointer rounded-full p-2 bg-indigo-500 text-white text-2xl hover:bg-indigo-700 absolute -bottom-2 -right-2 h-10 w-10"
+                  onClick={handleCopy}
+                >
+                  <MdContentCopy />
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
-      {passphrase}
-    </div>
+    </>
   );
 };
 
